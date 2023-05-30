a7ce1556728db923b0e3c3d674a99990418a5589
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-05-30 22:50:55 +0700

    30/05/23 Commit

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 if __name__ == '__main__':
-    print('This is to compile the KNN or Naive Bayes.')
+    data_filename = 'docs/arrhythmia.data'
+    name_filename = 'docs/arrhythmia.names'
+    try:
+        with open(data_filename, 'r') as file:
+            content = file.read()
+            print(content)
+    except IOError as e:
+        if isinstance(e, FileNotFoundError):
+            print(data_filename, 'not found!')
+        else:
+            print('An error occurred while reading the file: ', data_filename)
