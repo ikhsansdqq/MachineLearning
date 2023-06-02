b7c075ac25c6b74b6cb433eb51f6f5ff4efe3956
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-06-03 05:25:53 +0700

    adding files to vcs

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-if __name__ == '__main__':
-    data_filename = 'docs/arrhythmia.data'
-    name_filename = 'docs/arrhythmia.names'
-    try:
-        with open(data_filename, 'r') as file:
-            content = file.read()
-            print(content)
-    except IOError as e:
-        if isinstance(e, FileNotFoundError):
-            print(data_filename, 'not found!')
-        else:
-            print('An error occurred while reading the file: ', data_filename)
+import pandas as pd
+
+
+def read_csv(path):
+    data = pd.read_csv(path, delimiter=';', na_values='?')
+    return pd.DataFrame(data)
+
+
+def normalization_process(all_data, column_target):
+    for column in column_target:
+        all_data[column] = (all_data[column] - all_data[column].min()) / (
+                all_data[column].max() - all_data[column].min())
+    return all_data
+
+
+testStartID = data_train.index.stop
+all_data = normalization_process(data_train,
+                                 ['age', 'sex', 'height', 'weight', 'qrs_duration', 'qrs', 'QRST', 'heart_rate'])
+normTrain, normTest = all_data.iloc[:testStartID], all_data.iloc[testStartID:]
+
+data_train = read_csv('../docs/data_arrhythmia.csv')
