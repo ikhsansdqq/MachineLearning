95d19f6165b39dc2b96ad8d1a41353601695ca20
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-06-03 17:19:58 +0700

    adding files to vcs

--- main.py
+++ main.py
@@ -1,21 +1,2 @@
-import pandas as pd
-
-
-def read_csv(path):
-    data = pd.read_csv(path, delimiter=';', na_values='?')
-    return pd.DataFrame(data)
-
-
-def normalization_process(all_data, column_target):
-    for column in column_target:
-        all_data[column] = (all_data[column] - all_data[column].min()) / (
-                all_data[column].max() - all_data[column].min())
-    return all_data
-
-
-testStartID = data_train.index.stop
-all_data = normalization_process(data_train,
-                                 ['age', 'sex', 'height', 'weight', 'qrs_duration', 'qrs', 'QRST', 'heart_rate'])
-normTrain, normTest = all_data.iloc[:testStartID], all_data.iloc[testStartID:]
-
-data_train = read_csv('../docs/data_arrhythmia.csv')
+if __name__ == '__main__':
+    print("What")
